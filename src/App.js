c17e59dba31209b7fa6959dfd603bc942c357033
Carl Kiprono <carlkiprono00@gmail.com>
2025-10-23 17:18:38 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Conditional rendering to protect routes */}
+      {isLoggedIn ? <NavBar logout={logout} /> : <Navigate to="/login" />}
+      {/* Pass login function to Outlet as context */}
+      <Outlet context={login} />
     </div>
   );
 }
